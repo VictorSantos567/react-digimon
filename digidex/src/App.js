5b0498ffee2588285de62914ddbcc64a72bafece
Victor Augusto <vaugusto@lanum.com.br>
2022-11-30 18:34:51 -0300

    feat: adding project react

---
+++ digidex/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const url = "https://digimon-api.vercel.app/api/digimon/level/rookie";
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
