2c7b6c29dfb7ac0250d9777550c9e643c7d4d19e
VictorSantos567 <victoraugustos49@gmail.com>
2023-01-17 22:34:51 -0300

    feat: adding route digimons

--- digidex/src/App.js
+++ digidex/src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
 
-const url = "https://digimon-api.vercel.app/api/digimon/level/rookie";
+import { useState, useEffect } from "react";
+
+const url = "https://www.digi-api.com/api/v1/digimon";
 
 function App() {
+
+  const [digimons,setDigimons] = useState([]);
+
+  useEffect(() => {
+    async function fetchData(){
+      
+      const res = await fetch(url);
+      
+      const data = await res.json();
+
+      setDigimons(data);
+    }
+    fetchData();
+  },[]);
+
+  
+
   return (
     <div className="App">
-
+      <h1>Oi</h1>
+      {digimons}
     </div>
   );
 }
