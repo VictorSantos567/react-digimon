cb5ca7f3525d2151fd32e0cba65cdce2c8942deb
VictorSantos567 <victoraugustos49@gmail.com>
2023-01-18 21:15:28 -0300

    feat: adding componentes

--- digidex/src/App.js
+++ digidex/src/App.js
@@ -1,31 +1,14 @@
 import './App.css';
 
-import { useState, useEffect } from "react";
+import Digimon from './componentes/Digimon';
 
-const url = "https://www.digi-api.com/api/v1/digimon";
 
 function App() {
 
-  const [digimons,setDigimons] = useState([]);
-
-  useEffect(() => {
-    async function fetchData(){
-      
-      const res = await fetch(url);
-      
-      const data = await res.json();
-
-      setDigimons(data);
-    }
-    fetchData();
-  },[]);
-
-  
-
   return (
     <div className="App">
-      <h1>Oi</h1>
-      {digimons}
+      <h1>Digidex</h1>
+      <Digimon />
     </div>
   );
 }
---
+++ digidex/src/componentes/Digimon/index.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+
+const url = "https://www.digi-api.com/api/v1/digimon";
+
+const Digimon = () => {
+    
+    const [digimons,setDigimons] = useState([]);
+
+    useEffect(() => {
+      async function fetchData(){
+        
+        const res = await fetch(url);
+        
+        const data = await res.json();
+
+        const response = data.content
+
+        setDigimons(response);
+      }
+      fetchData();
+    },[]);
+
+
+    return (
+    <div>
+      <ul>
+        {digimons.map((digimon)=>
+          <li key={digimon.id}>
+            {digimon.name}
+          </li>
+        )}
+      </ul>
+    </div>
+    );
+}
+
+export default Digimon;
