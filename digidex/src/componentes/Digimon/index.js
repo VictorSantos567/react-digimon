b2274f6f0ccf6a1ece81dd90e0751734dab6b9f5
VictorSantos567 <victoraugustos49@gmail.com>
2023-01-31 23:00:55 -0300

    feat: show response digimon text

--- digidex/src/componentes/Digimon/index.js
+++ digidex/src/componentes/Digimon/index.js
@@ -1,32 +1,30 @@
 import { useState, useEffect } from "react";
 
-const url = "https://www.digi-api.com/api/v1/digimon";
+const url = "https://www.digi-api.com/api/v1/digimon"
 
 const Digimon = (props) => {
   const [digimons, setDigimons] = useState([]);
 
+  const urlName = url+"/"+props.teste
+
   useEffect(() => {
     async function fetchData() {
-      const res = await fetch(url);
 
-      const data = await res.json();
+      const res = await fetch(urlName);
 
-      const response = data.content;
+      const data = await res.text();
+
+      const response = data;
 
       setDigimons(response);
     }
     fetchData();
-  }, []);
+  },);
 
   return (
     <div>
-      <h1>List Digimon</h1>
-      <ul>
-        {digimons.map((digimon) => (
-          <li key={digimon.id}>{digimon.name}</li>
-        ))}
-      </ul>
       <h1>{props.teste}</h1>
+      <h2>{digimons}</h2>
     </div>
   );
 };
